336235d82969c35d0e9e1f5fb7e0c94efacdd951
theoverse <theoverse@protonmail.com>
2022-03-07 21:22:06 -0600

    added styles and logo image, and added design to navigation bar

--- src/component/header/Navigation.jsx
+++ src/component/header/Navigation.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import { Container, Navbar, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import '../../assets/css/main.css'
+import Logo from '../../assets/images/logo.png'
 
 const Navigation = () => {
     return (
         <div>
-            <Navbar bg="dark" variant="dark">
+            <Navbar bg="dark" expand="lg">
                 <Container>
-                    <Navbar.Brand href="#home">Easy Eats</Navbar.Brand>
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
-                    </Nav>
+                    <Navbar.Brand><Link to="/"><img src={Logo} height="64px" width="192px" /></Link></Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link><Link to="/" className="main-menu">Home</Link></Nav.Link>
+                            <Nav.Link><Link to="/menu" className="main-menu">Menu</Link></Nav.Link>
+                            <Nav.Link><Link to="/about" className="main-menu">About</Link></Nav.Link>
+                            <Nav.Link><Link to="/contact" className="main-menu">Contact</Link></Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
                 </Container>
             </Navbar>
         </div>
